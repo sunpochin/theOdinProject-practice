05c6dabc48c80c597b5133428822d3c8eef95ec3
pac <sunpochin@gmail.com>
2022-08-02 23:56:18 +0800

    adding background img, adding flexbox

--- weather-app/script.js
+++ weather-app/script.js
@@ -1,11 +1,30 @@
-// https://www.theodinproject.com/lessons/node-path-javascript-library
+// https://www.theodinproject.com/lessons/node-path-javascript-weather-app
 
-let myLibrary = [];
-
-function Book() {
-	// the constructor...
-}
-
-function addBookToLibrary() {
+async function getWeather() {
 	// do stuff here
+	let cityname = 'taipei';
+	let address = '';
+	let response;
+	let lat = '25.105497';
+	let lon = '121.597366';
+	let weatherData;
+	let xx = '48017ab2e468af17b4c9fcca0c5e2fda';
+	address =
+		`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&APPID=${xx}`;
+
+	response = await fetch(address, { mode: 'cors' });
+	weatherData = await response.json();
+	console.log(weatherData);
+
+	const temp = document.getElementById('temperature');
+	temp.innerText = weatherData.weather[0].main;
+
+	const apiCall = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${xx}`;
+
+	response = await fetch(address, { mode: 'cors' });
+	weatherData = await response.json();
+	console.log('weatherData:', weatherData);
+
 }
+
+getWeather();
