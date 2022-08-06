255a7aff3ea46bb340a807f40ff105cf4202ef66
pac <sunpochin@gmail.com>
2022-08-06 17:59:11 +0800

    adding component

--- js-prac/task-app/src/App.js
+++ js-prac/task-app/src/App.js
@@ -1,10 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React, { Component } from 'react';
+import uniqid from 'uniqid';
+import Overview from './components/Overview';
 
-function App() {
-  return (
-    <div>Hello World</div>
-  );
+class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			task: {
+				text: '',
+				id: uniqid(),
+			},
+			tasks: [],
+		};
+	}
+
+	handleChange = (e) => {
+		console.log('value: ', e.target.value);
+		this.setState({
+			task: {
+				text: e.target.value,
+				id: this.state.task.id,
+			},
+		});
+	};
+
+	onSubmitTask = (e) => {
+		e.preventDefault();
+		this.setState({
+			tasks: this.state.tasks.concat(this.state.task),
+			task: {
+				text: '',
+				id: uniqid(),
+			},
+		});
+	};
+
+	render() {
+		const { task, tasks } = this.state;
+
+		return (
+			<div>
+				<form onSubmit={this.onSubmitTask}>
+          <Overview tasks={tasks} />
+					<label htmlFor='taskInput'>Enter task</label>
+					<input
+						onChange={this.handleChange}
+						value={task.text}
+						type='text'
+						id='taskInput'
+					/>
+					;<button type='submit'>Add Task</button>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default App;
+
+// App.js
+// // import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div>Hello World</div>
+//   );
+// }
+
+// export default App;
---
+++ js-prac/task-app/src/components/Overview.js
@@ -0,0 +1,17 @@
+// Overview.js
+
+import React from 'react';
+
+const Overview = (props) => {
+	const { tasks } = props;
+
+	return (
+		<ul>
+			{tasks.map((task) => {
+				return <li>{task.text}</li>;
+			})}
+		</ul>
+	);
+};
+
+export default Overview;
